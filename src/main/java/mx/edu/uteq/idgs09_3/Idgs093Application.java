554379617ed78fcb398a9138b28844904fbcbf88
Derck23 <eramireznieves25@gmail.com>
2025-06-23 17:49:51 -0600

    proyecto

---
+++ src/main/java/mx/edu/uteq/idgs09_3/Idgs093Application.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs09_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idgs093Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idgs093Application.class, args);
+	}
+
+}
